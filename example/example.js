1422b317262df3f1b6ceedd26cb293a55bc2a6ed
Branden Horiuchi <bhoriuchi@gmail.com>
2015-06-25 13:30:31 -0700

    fixing jslint warnings

--- example/example.js
+++ example/example.js
@@ -5,47 +5,39 @@
 var dotprune = require('../lib/prune');
 
 // define an array of objects to prune
-var obj = [
-    {
-    	id: 1,
-    	name: "Jack Shepard",
-    	groups: [
-    	    {
-    	    	id: 1,
-    	    	name: "survivors",
-    	    	station: {
-    	    		id: 2,
-    	    		name: "swan"
-    	    	}
-    	    },
-    	    {
-    	    	id: 2,
-    	    	name: "chosen ones",
-    	    	station: {
-    	    		id: 5,
-    	    		name: "temple"
-    	    	}
-    	    }
-    	]
-    },
-    {
-    	id: 3,
-    	name: "Ben Linus",
-    	groups: [
-    	    {
-    	    	id: 3,
-    	    	name: "others",
-    	    	station: {
-    	    		id: 20,
-    	    		name: "others camp"
-    	    	}
-    	    }
-    	]
-    }
-];
+var obj = [ {
+	id : 1,
+	name : "Jack Shepard",
+	groups : [ {
+		id : 1,
+		name : "survivors",
+		station : {
+			id : 2,
+			name : "swan"
+		}
+	}, {
+		id : 2,
+		name : "chosen ones",
+		station : {
+			id : 5,
+			name : "temple"
+		}
+	} ]
+}, {
+	id : 3,
+	name : "Ben Linus",
+	groups : [ {
+		id : 3,
+		name : "others",
+		station : {
+			id : 20,
+			name : "others camp"
+		}
+	} ]
+} ];
 
-
-var prunedObject = dotprune.prune(obj, ['id', 'name', 'groups.name', 'groups.station']);
+var prunedObject = dotprune.prune(obj, [ 'id', 'name', 'groups.name',
+		'groups.station' ]);
 var prettyObject = JSON.stringify(prunedObject, null, '  ');
 
 console.log(prettyObject);
